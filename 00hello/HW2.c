c8b2a8c9727f35d9f158dc12f6f97132e85fb5e7
Wangpanic <411121261@gms.ndhu.edu.tw>
2023-12-23 00:44:10 +0800

    modified

--- 00hello/HW2.c
+++ 00hello/HW2.c
@@ -1,41 +1,70 @@
 #include <stdio.h>
 
-
-int can(int color ,int x, int y, int chess[8][8]) {
-    int an_color = 3-color;
-    for(int i = -1; i <= 1; i ++) {
-        for(int j = -1; j <= 1; j ++) {
-            if(x+i < 8 && x+i >= 0 && y+j < 8 && y+j >= 0 && chess[x+i][y+j] == an_color) {
-                int cx = x+i, cy = y+j;
-                while(cx < 8 && cx >= 0 && cy < 8 && cy >= 0) {
-                    if(chess[cx][cy] == 0)
-                        break;
-                    if(chess[cx][cy] == color)
-                        return 1;
-                    cx = cx+i;
-                    cy = cy+j;
-                }
-           }
+#define block 8
+int main(){
+    /*
+    int chess[8][8] = {
+    {2,1,1,1,1,1,1,0},
+    {2,1,2,1,1,2,1,0},
+    {2,2,1,1,1,2,1,1},
+    {2,1,2,1,1,2,1,0},
+    {2,1,2,2,2,2,1,0},
+    {2,2,2,2,2,2,1,0},
+    {2,2,2,1,1,2,0,0},
+    {0,1,0,0,0,0,0,0}
+    };
+    */
+    printf("0 block 1 black 2 white\n");
+    for(int i=0 ; i<block ; i++){
+        for(int j=0 ; j<block ; j++){
+            scanf("%d", &chess[i][j]);
         }
     }
-    return 0;
-}
-
-int main(){
-    int x, y;
-    int chess[8][8];
-    for(int i = 0; i < 8; i ++)
-        for(int j = 0; j < 8; j ++)
-            scanf("%d" , &chess[i][j]);
-    scanf("%d %d", &x, &y);
+    int x , y , dx , dy , rx , ry;
+    scanf("%d %d", &x , &y);
     
-    for(int i = -1; i <= 1; i ++) {
-        for(int j = -1; j <= 1; j ++) {
-            if((x+j) >= 0 && (y+i) >= 0 && (y+i) < 8 && (x+j) < 8) {
-                if(chess[x+i][y+j] == 0 && can(chess[x][y] ,x+i, y+j, chess) == 1)
-                    printf("(%d %d) ", x+i, y+j);
+    if(x<0 || y<0 || x>=block || y>=block){
+        printf("Not in range.");
+    }
+    else if(chess[y][x] == 0){
+        printf("it is block.");
+    }
+    else{
+        for(dx=-1 ; dx<=1 ; dx++){
+            for(dy=-1 ; dy<=1 ; dy++){
+                if(dx != 0 || dy != 0){
+                rx = x;
+                ry = y;
+                rx += dx;
+                ry += dy;
+                for(int i=0 ; i<6 ; i++){
+                    if(chess[y][x] != chess[ry][rx] && chess[ry][rx] != 0){
+                        rx += dx;
+                        ry += dy;
+                    }
+                    else if(chess[ry][rx] == 0 && chess[ry-dy][rx-dx] != chess[y][x]){
+                        chess[ry][rx] = 3;
+                        break;
+                    }
+                    else{break;}
+                }
+                }
             }
         }
     }
+    for(int i=0 ; i<block ; i++){
+        for(int j=0 ; j<block ; j++){
+            
+            printf("%d ",chess[i][j]);
+            
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
+
+
+
+
+
